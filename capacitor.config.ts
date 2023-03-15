c4e7a5f33dbacfccdad8ec80807e4a5318af30b8
demithehomie <demithehomie@gmail.com>
2023-03-15 16:00:59 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lampada',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
