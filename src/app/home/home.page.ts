09ade6a1383da118094201dbc059d183e6593657
demithehomie <demithehomie@gmail.com>
2023-03-15 16:24:20 -0300

    Lâmpada feita com Angular e Ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,18 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+ // constructor() {}
 
+ interruptor(): void {
+  const imagem = (<HTMLImageElement>document.getElementById('lamp')).src;
+
+  const imagem_ligado = 'https://www.w3schools.com/js/pic_bulbon.gif';
+  const imagem_desligado = 'https://www.w3schools.com/js/pic_bulboff.gif';
+
+  if (imagem === imagem_ligado) {
+    (<HTMLImageElement>document.getElementById('lamp')).src = imagem_desligado;
+  } else {
+    (<HTMLImageElement>document.getElementById('lamp')).src = imagem_ligado;
+  }
+}
 }
